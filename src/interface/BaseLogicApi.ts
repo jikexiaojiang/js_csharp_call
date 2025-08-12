dcbdd5b0d73fa7ca6470e2937b332ae504b5ebf9
zack-zhang_HON <zack.zhang@honeywell.com>
2025-08-12 16:39:29 +0800

    update

--- src/interface/BaseLogicApi.ts
+++ src/interface/BaseLogicApi.ts
@@ -6,6 +6,7 @@ export abstract class BaseLogicAPI implements ILogicAPI {
     abstract getConfig(num: number): Promise<any>;
     abstract setConfig(config: any): Promise<any>;
     abstract getProperty(): string;
+    abstract onResponse(response: string): void;
     protected cppfBridge: ICPPFBridge;
 
     public registerCPPFBridge = (cppfBridge: ICPPFBridge) => {
